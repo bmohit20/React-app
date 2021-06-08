f2c279010a17833d484adb705a777bd3c2d97671
Mohit bhatt <mohit.b.bhatt@accenture.com>
2021-06-08 17:09:01 +0530

    Second attempt

--- src/BlockComponent.jsx
+++ src/BlockComponent.jsx
@@ -4,9 +4,9 @@ import "./DemoComponent.css";
 
 const BlockComponent = (props) => {
   return (
-    <div className="block-data-container">
-      <h1 ClassName="block-name">{props.block.name}</h1>
-      <p className="block-desc">{props.block.desc}</p>
+    <div className="block-data-container col-xs-4" key={props.block.id}>
+      <h1 className="block-name">{props.block.name}</h1>
+      <p className="block-desc">{props.block.DOB}</p>
     </div>
   );
 };
--- src/DemoComponent.jsx
+++ src/DemoComponent.jsx
@@ -3,37 +3,68 @@ import React from "react";
 import "./DemoComponent.css";
 // Component
 import BlockComponent from "./BlockComponent";
+// Json
+import Demo from './Demo.json';
 
 class DemoComponent extends React.Component {
   constructor() {
     super();
     this.state = {
       blocks: [],
+      loadedBlocks: 3
     };
   }
 
   componentDidMount = () => {
     this.getData().then((data) => {
+      console.log(data);
       this.setState({ blocks: data });
     });
   };
 
   getData = () => {
+    // API Call, Still getting CORS issue while fatching the data
+    return fetch("http://blockchain.info/blocks/1328830483000?format=json", {
+      headers: {cors: true, "Access-Control-Allow-Origin": "*" },
+    }).then((res) => {
+      return Promise.resolve(res);
+    }).catch((err) => {
+      // Fallback UI here
+      return Promise.resolve(Demo.blocks)
+    });
     // Call service here using promise as a return and with try catch block.
-    return Promise.resolve([]);
   };
 
+  viewMore = () => {
+    if (this.state.loadedBlocks < this.state.blocks.length) {
+      this.setState((prevState) => ({
+        loadedBlocks: prevState.loadedBlocks + 3
+      }))
+    } else {
+      this.setState((prevState) => ({
+        loadedBlocks: 3
+      }))
+    }
+  }
+
   getBlock = () => {
-    this.state.blocks.map((block) => {
-      return <BlockComponent block={block}></BlockComponent>;
+    return this.state.blocks && this.state.blocks.length && this.state.blocks.map((block, index) => {
+      if (index >= this.state.loadedBlocks) {
+        return '';
+      }
+      return <BlockComponent block={block} key = {block.id}></BlockComponent>;
     });
   };
+
   render = () => {
     return (
       <React.Fragment>
         <div className="block-data container">
           <h1 className="data-heading col-xs-12">Data Heading</h1>
           <div className="block-element col-xs-12">{this.getBlock()}</div>
+          <div className='button-container'>
+          <button className='view-more' onClick={this.viewMore}>{this.state.loadedBlocks < this.state.blocks.length ? 'View More' : 'View Less'}</button>
+          </div>
         </div>
       </React.Fragment>
     );
